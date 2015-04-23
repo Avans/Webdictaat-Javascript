96ef70c413508328dff828edd3f2b06e11bace31
Paul Wagener <mail@paulwagener.nl>
2015-04-23 18:34:12 +0200

    Directive challenges

--- app/app.js
+++ app/app.js
@@ -17,8 +17,8 @@ app.factory('UserFactory', function($http){
 
 	userFactory.user = {
 		isLoggedIn: false,
-		username: 'Paul',
-		points: 5
+		username: '',
+		points: 0
 	};
 
 	userFactory.login = function() {
@@ -30,7 +30,81 @@ app.factory('UserFactory', function($http){
 		userFactory.user.isLoggedIn = false;
 	};
 
+	userFactory.completeAssignment = function(assignmentId, points) {
+		$http.post(BASE_URL + '/Points?token=' + window.localStorage['token'], {assignmentId: assignmentId});
+		console.log(assignmentId, points);
+	};
+
 	return userFactory;
+});
+
+app.directive('spChallenge', function($compile, UserFactory) {
+	return {
+		restrict: 'E',
+		scope: {
+			assignment: '@'
+		},
+		templateUrl: 'challenge.html',
+		link: function(scope, element, attrs) {
+			// Make it an Ace editor
+			var js_editor = element.find('.editor.js')
+				.text(window.localStorage['source.' + scope.a.id])
+				.height(150);
+            ace_editor = get_editor(js_editor[0]);
+        	ace_editor.getSession().setMode("ace/mode/javascript");
+        	js_editor.append(element.find('.run'));
+
+        	scope.js_editor = ace_editor;
+        	scope.element = element;
+		},
+		controller: function($scope) {
+			var assignment = assignments[$scope.assignment];
+			$scope.a = assignment;
+
+			$scope.run = function() {
+				var user_code = $scope.js_editor.getSession().getValue();
+
+                // Save code in browser
+                window.localStorage['source.' + assignment.id] = user_code;
+
+                // Set up an iframe to run code in
+                var iframe = $('<iframe>');
+                $('body').append(iframe);
+                iframe.contents().find('body').append(assignment.html);
+                var frameWindow = iframe[0].contentWindow;
+
+                try {
+                    // Run the code!
+                    frameWindow.eval(user_code);
+
+                    // Test the return value
+                    var returnValue = frameWindow.eval(assignment.return);
+                    assignment.tester(returnValue);
+
+                    // No error, complete the assignment!
+                    UserFactory.completeAssignment(assignment.id, assignment.points);
+
+                    $scope.completed = true;
+                    $scope.element.find('.live_assignment').addClass('completed');
+                    $scope.element.find('.complete')
+                        .slideDown()
+                        .delay(1000)
+                        .slideUp();
+
+                } catch(e) {
+                    // Show error to user
+                    $scope.element.find('.error').html("Error: " + e)
+                        .slideDown()
+                        .delay(3000)
+                        .slideUp();
+                } finally {
+                    iframe.remove();
+                }
+			}
+
+
+		}
+	};
 });
 
 
@@ -53,7 +127,7 @@ app.directive('myDailyQuest', function() {
     		if(answer == $scope.correctAnswer){
 
     		}
-    	}	
+    	}
     }
   };
 });
@@ -91,7 +165,6 @@ app.controller("LeaderboardController", ["$scope", "$http", "UserFactory", funct
 
 	$http.get(BASE_URL + '/Leaderboards?token=' + window.localStorage['token'])
 		.success(function(data, status){
-			console.log(data);
 			$scope.leaderboard = data;
 	});
 }]);
