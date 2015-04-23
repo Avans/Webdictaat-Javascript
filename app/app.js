9a9a746a11658b5423979852f14e599d118de63f
Linksonder <Linksonder@gmail.com>
2015-04-23 15:23:55 +0200

    DailyQuestStart

--- app/app.js
+++ app/app.js
@@ -20,6 +20,31 @@ app.factory('PointFactory', function($http){
 	return pointFactory;
 });
 
+app.directive('myDailyQuest', function() {
+  return {
+  	restrict: 'E',
+    templateUrl: 'dailyQuest/quest.html',
+    controller: function($scope){
+
+    	$scope.question = "What is 1 + 1?";
+    	$scope.answers = [
+    		{char: 'A', value: 2, correct: true },
+    		{char: 'B', value: 5, correct: false },
+    		{char: 'C', value: 8, correct: false },
+    		{char: 'D', value: 3, correct: false }
+    	];
+    	$scope.correctAnswer = 5;
+
+    	$scope.submitAnswer = function(answer){
+    		if(answer == $scope.correctAnswer){
+
+    		}
+    	}	
+    }
+  };
+});
+
+
 app.controller("LeaderboardController", ["$scope", "$http", "PointFactory", function($scope, $http, PointFactory){
 
 	$scope.isLoggedIn = false;
